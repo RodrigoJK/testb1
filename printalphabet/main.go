1cb8b041f032f9ef9bc719959146b0c7bcf62ba1
Rodrigo Luyckx <luyckxrodrigo@gmail.com>
2024-09-03 16:50:13 +0200

    printalphabet

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -3,9 +3,8 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	var a int = 97
-	for i := 0; i < 26; i++ {
-		z01.PrintRune(rune(a + i))
+	for i := 'a'; i <= 'z'; i++ {
+		z01.PrintRune(i)
 	}
 	z01.PrintRune('\n')
 }
